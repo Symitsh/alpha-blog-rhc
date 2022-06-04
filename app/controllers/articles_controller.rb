1498c6f589a56e6bf0a9852f3cafa575aca0c60f
Sandri Florent <sandri822@hotmail.fr>
2022-06-04 17:05:03 +0200

    🗃  Action show and new complete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,22 @@
 class ArticlesController < ApplicationController
-
   def new
     @article = Article.new
   end
 
   def create
-    #render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+    if @article.save
+      # On affiche l'article
+      flash[:notice] = "Article bel et bien crée"
+      redirect_to article_path(@article)
+    else
+      # ça n'a pas marcher
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 
   private
@@ -16,5 +24,4 @@ class ArticlesController < ApplicationController
   def article_params
     params.require(:article).permit(:title, :description)
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,4 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
