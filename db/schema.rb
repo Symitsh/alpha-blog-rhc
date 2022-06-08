e4dc67948c5697daf7965884c1d96dcab8380869
Sandri Florent <sandri822@hotmail.fr>
2022-06-08 14:58:20 +0200

    🗃  Create users & model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_03_061750) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_08_124636) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.text "description"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
