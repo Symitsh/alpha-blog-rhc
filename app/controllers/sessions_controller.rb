b9feffeb9581c9c99a1dd524a5c2a22d609962f6
Sandri Florent <sandri822@hotmail.fr>
2022-06-11 16:05:40 +0200

    ✨ Create and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,16 +5,18 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      log_in user
-      redirect_to user
+      flash[:success] = 'Bon retour parmis nous!'
+      session[:user_id] = user.id
+      redirect_to user_path(user)
     else
-      flash.now[:danger] = 'Invalid email/password combination'
-      render 'new'
+      flash.now[:danger] = 'Identifiants incorrects'
+      render 'new', status: :unprocessable_entity
     end
   end
 
   def destroy
-    log_out
-    redirect_to root_url
+    session[:user_id] = nil
+    flash[:sucess] = 'A bientot!'
+    redirect_to root_path
   end
 end
